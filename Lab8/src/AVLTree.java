e4f6435eb574b1b49af6ca3a09beabcae2748426
jgore077 <jgore077@gmail.com>
2023-03-29 10:42:43 -0400

    add method done

--- Lab8/src/AVLTree.java
+++ Lab8/src/AVLTree.java
@@ -13,7 +13,11 @@ public class AVLTree {
         }
     } 
     public static void main(String[] args) {
-         	
+         	AVLTree tree = new AVLTree();
+            int[] list ={72, 10, 25, 63, 91, 4, 85, 50, 13, 44, 57, 88, 97, 39, 78, 30, 21, 48, 65, 99, 60, 76, 33, 55, 82};
+	        for(int i=0;i<list.length;i++){
+		        tree.add(list[i]);
+	         }
     }
     public void rotateLeft(Node node){
 
@@ -21,17 +25,41 @@ public class AVLTree {
     public void rotateRight(Node node){
         
     }
+    //calculate balance factor
     public int calculateNode(Node node){
         return 0;
     }
-    public void add(int item){
-
+    public boolean add(int item){
+        if (root == null) {
+            root = new Node(item);
+            return true;
+        } else {
+            // If the root is not null, call the addHelper method to recursively add the element to the tree
+            return add(root,item);
+        }
     }
-
+    private boolean add(Node node,int item){
+        if (item<node.val) {
+            if (node.left == null) {
+                node.left = new Node(item);
+                return true;
+            } else {
+                return add(node.left,item);
+            }
+        } 
+        else if (item>node.val) {
+            if (node.right == null) {
+                node.right = new Node(item);
+                return true;
+            } else {
+                return add(node.right,item);
+            }
+        } 
+        else {
+            return false;
+        }
+    }
     
-    private void add(Node node,int item){
-
-    }
     public int height(Node node){
     	if(node == null) {
     		return 0;
